c9871e63d6bd366a35c96a6c397d828713e3fe2f
IrishBansal32 <148973763+IrishBansal32@users.noreply.github.com>
2023-11-28 02:51:13 +0530

    Add files via upload

---
+++ Crypto Portfolio/script.js
@@ -0,0 +1,23 @@
+// Function to open the login modal
+function openLoginModal() {
+    document.getElementById("loginModal").style.display = "block";
+}
+
+// Function to close the login modal
+function closeLoginModal() {
+    document.getElementById("loginModal").style.display = "none";
+}
+
+// Function to open the signup modal
+function openSignupModal() {
+    document.getElementById("signupModal").style.display = "block";
+}
+
+// Function to close the signup modal
+function closeSignupModal() {
+    document.getElementById("signupModal").style.display = "none";
+}
+
+
+
+
